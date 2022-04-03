26c5d4a779c92ca2cdc7f1988958ec99f3294c87
Jacob <Jacob.McCann214@gmail.com>
2022-04-03 18:39:33 -0500

    Completed Assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,5 +7,8 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
+    // debugging tool
+    screen.debug();
+
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
